7585d6484c224fc535d34566fdc51cff1348ee8f
Arzoo-Rai <arzoo.nitp48@gmail.com>
2021-02-09 21:31:12 +0530

    Add files via upload

--- employeeserv-master/employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/impl/EmployeeResourceImpl.java
+++ employeeserv-master/employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/impl/EmployeeResourceImpl.java
@@ -4,6 +4,8 @@ import com.paypal.bfs.test.employeeserv.api.EmployeeResource;
 import com.paypal.bfs.test.employeeserv.api.model.Employee;
 import com.paypal.bfs.test.employeeserv.repository.EmployeeJpaRepository;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +22,17 @@ public class EmployeeResourceImpl implements EmployeeResource {
 
     @Override
     public ResponseEntity<Employee> employeeGetById(String id) {
-
-        Employee employee = new Employee();
-        employee.setId(Integer.valueOf(id));
-        employee.setFirstName("BFS");
-        employee.setLastName("Developer");
+    	Optional<com.paypal.bfs.test.employeeserv.entity.Employee> emp = empRepository.findById(Integer.parseInt(id));
+        Employee employee= new Employee();
+        employee.setId(emp.get().getId());
+		employee.setFirstName(emp.get().getFirstName());
+		employee.setLastName(emp.get().getLastName());
+		employee.setAddressLine1(emp.get().getAddressLine1());
+		employee.setAddressLine2(emp.get().getAddressLine2());
+		employee.setZip(emp.get().getZip());
+		employee.setState(emp.get().getState());
+		employee.setCountry(emp.get().getCountry());
+		
 
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
